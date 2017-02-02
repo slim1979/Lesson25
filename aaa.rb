d4c20d4e99e3ced4ed2161ad47645efb485dc0d5
chinezan <chinezan@yandex.ru>
2017-02-02 23:33:13 +0400

    добавил более сложный запрос к базе данных

--- aaa.rb
+++ aaa.rb
@@ -1,6 +1,12 @@
 require 'sqlite3'
 db = SQLite3::Database.new 'new_base.db'
+#простой запрос на добавление сроки в базу данных
+#пока закомментирован, чтобы не удалять, чтобы был
+#db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 13450)"
 
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 13450)"
-
+#чуть сложнее запрос
+db.execute "SELECT * From Cars" do |car|
+	puts car
+	puts "===="
+end
 db.close
